439e9d94152b11b18635f655a8aa92e620eb9c9f
applenick <applenick@users.noreply.github.com>
2021-10-12 00:51:59 -0700

    Expose player time with match end event
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/tc/oc/occ/dispense/events/match/PGMMatchEndEvent.java
+++ src/main/java/tc/oc/occ/dispense/events/match/PGMMatchEndEvent.java
@@ -2,6 +2,7 @@ package tc.oc.occ.dispense.events.match;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.bukkit.entity.Player;
 import tc.oc.occ.dispense.events.DispenseEvent;
@@ -10,11 +11,17 @@ public class PGMMatchEndEvent extends DispenseEvent {
 
   private final List<Player> winners;
   private final List<Player> participation;
+  private final Map<Player, Duration> playerTimes;
   private final Duration matchLength;
 
-  public PGMMatchEndEvent(List<Player> winners, List<Player> participation, Duration matchLength) {
+  public PGMMatchEndEvent(
+      List<Player> winners,
+      List<Player> participation,
+      Map<Player, Duration> playerTimes,
+      Duration matchLength) {
     this.winners = winners;
     this.participation = participation;
+    this.playerTimes = playerTimes;
     this.matchLength = matchLength;
   }
 
@@ -26,6 +33,10 @@ public class PGMMatchEndEvent extends DispenseEvent {
     return participation.stream()
         .filter(p -> includeWinners || !winners.contains(p))
         .collect(Collectors.toList());
+  }
+
+  public Duration getPlayerTime(Player player) {
+    return playerTimes.get(player);
   }
 
   public Duration getMatchLength() {
