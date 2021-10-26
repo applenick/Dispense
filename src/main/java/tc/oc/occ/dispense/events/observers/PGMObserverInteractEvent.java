e860e8200cc501d3351e49f1f93d47fe74abd8b8
applenick <applenick@users.noreply.github.com>
2021-10-25 18:26:28 -0700

    Support observer player interactions
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/tc/oc/occ/dispense/events/observers/PGMObserverInteractEvent.java
+++ src/main/java/tc/oc/occ/dispense/events/observers/PGMObserverInteractEvent.java
@@ -1,19 +1,34 @@
 package tc.oc.occ.dispense.events.observers;
 
+import javax.annotation.Nullable;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import tc.oc.occ.dispense.events.SinglePlayerEvent;
 
 public class PGMObserverInteractEvent extends SinglePlayerEvent {
 
-  private ItemStack item;
+  private final ItemStack item;
+  private final @Nullable Player target;
+  private final boolean isTargetObserver;
 
-  public PGMObserverInteractEvent(Player player, ItemStack item) {
+  public PGMObserverInteractEvent(
+      Player player, ItemStack item, Player target, boolean isTargetObserver) {
     super(player);
     this.item = item;
+    this.target = target;
+    this.isTargetObserver = isTargetObserver;
   }
 
   public ItemStack getItem() {
     return item;
+  }
+
+  @Nullable
+  public Player getTargetPlayer() {
+    return target;
+  }
+
+  public boolean isTargetObserver() {
+    return isTargetObserver;
   }
 }
