80cc7cf770743846d99c5226a4d805fc43ff1d79
applenick <applenick@users.noreply.github.com>
2023-09-24 11:07:57 -0700

    Add restart events
    Signed-off-by: applenick <applenick@users.noreply.github.com>

---
+++ src/main/java/tc/oc/occ/dispense/events/restart/PGMRestartEvent.java
@@ -0,0 +1,8 @@
+package tc.oc.occ.dispense.events.restart;
+
+import tc.oc.occ.dispense.events.DispenseEvent;
+
+public class PGMRestartEvent extends DispenseEvent {
+
+  public PGMRestartEvent() {}
+}
