54c8ee31df4326b562a4fd4a593e966994e4754d
applenick <applenick@users.noreply.github.com>
2023-09-24 11:33:51 -0700

    Add additional fields to PGMRestartEvent
    Signed-off-by: applenick <applenick@users.noreply.github.com>

--- src/main/java/tc/oc/occ/dispense/events/restart/PGMRestartEvent.java
+++ src/main/java/tc/oc/occ/dispense/events/restart/PGMRestartEvent.java
@@ -1,8 +1,23 @@
 package tc.oc.occ.dispense.events.restart;
 
+import java.time.Duration;
 import tc.oc.occ.dispense.events.DispenseEvent;
 
 public class PGMRestartEvent extends DispenseEvent {
 
-  public PGMRestartEvent() {}
+  private Duration remaining;
+  private int online;
+
+  public PGMRestartEvent(Duration remaining, int online) {
+    this.remaining = remaining;
+    this.online = online;
+  }
+
+  public Duration getRemaining() {
+    return remaining;
+  }
+
+  public int getOnline() {
+    return online;
+  }
 }
