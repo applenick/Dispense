b62235671bbc6db9c2936574744dab3b9ade7322
applenick <applenick@users.noreply.github.com>
2025-01-03 00:37:53 -0800

    Add PGMRefreshMapDataEvent
    Signed-off-by: applenick <applenick@users.noreply.github.com>

---
+++ src/main/java/tc/oc/occ/dispense/events/map/PGMRefreshMapDataEvent.java
@@ -0,0 +1,8 @@
+package tc.oc.occ.dispense.events.map;
+
+import tc.oc.occ.dispense.events.DispenseEvent;
+
+public class PGMRefreshMapDataEvent extends DispenseEvent {
+
+  public PGMRefreshMapDataEvent() {}
+}
