5993d1934aa6f61779b3f3e364a4526722e3062e
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2026-01-29 16:52:33 -0800

    Build against SportPaper-Paper intersect jar (#3)

--- src/main/java/tc/oc/occ/dispense/events/players/PGMPlayerDeathEvent.java
+++ src/main/java/tc/oc/occ/dispense/events/players/PGMPlayerDeathEvent.java
@@ -1,7 +1,7 @@
 package tc.oc.occ.dispense.events.players;
 
-import javax.annotation.Nullable;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 import tc.oc.occ.dispense.events.DispenseEvent;
 
 public class PGMPlayerDeathEvent extends DispenseEvent {
